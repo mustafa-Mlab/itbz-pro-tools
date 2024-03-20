a7c07f9193ba2d1f3f4133e20e35e163e2ab7785
mustafa-Mlab <mkamalhosssain@gmail.com>
2024-03-21 01:57:29 +0600

    removed unnecessery codes

--- includes/activator.php
+++ includes/activator.php
@@ -30,28 +30,4 @@ function itbz_pro_tools_create_tools_manager_role() {
 }
 
 
-// Create custom table for credit transactions
-// function create_credit_transactions_table() {
-//   global $wpdb;
-
-//   // $table_name = $wpdb->prefix . 'credit_transactions';
-//   $table_name = $wpdb->prefix . 'itbz_pro_tools_credit_transactions';
-
-//   $charset_collate = $wpdb->get_charset_collate();
-
-//   $sql = "CREATE TABLE $table_name (
-//       transaction_id bigint(20) NOT NULL AUTO_INCREMENT,
-//       user_email varchar(100) NOT NULL,
-//       credits_amount int NOT NULL,
-//       transaction_date datetime NOT NULL,
-//       transaction_type varchar(20) NOT NULL, 
-//       PRIMARY KEY  (transaction_id)
-//   ) $charset_collate;";
-
-//   require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-//   dbDelta($sql);
-// }
-
-
-
 itbz_pro_tools_activate();
